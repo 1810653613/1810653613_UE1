79161a4fd3d2da3d9db865045ca7002e4a45d55c
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-11-17 18:30:02 +0100

    Uebung 2 Variablen und Konstanten

---
+++ src/Aufgab2_1810653613.java
@@ -0,0 +1,19 @@
+public class Aufgab2_1810653613
+{
+    public static void main(String[] args)
+    {
+    String name ="Max"; // Der Wertebereich sind Buchstaben
+    int alter=21;   // Der Wertebereich sind ganze Zahlen von ca. -2Mrd. bis 2Mrd.
+    double KommazahlDoppelt=0.999;  //Der Wertebereich sind Fließkommazahlen mit doppelter Genauigkeit
+    boolean Wahrheit=false; // Der Wertebereich sind Wahrheitswerte true oder false
+    long großeZahl=11111111;    //Der Wertebereich sind ganze Zahlen von -2^63 bis 2^63-1
+    char Variable='x';  //Der Wertebereich sind Zeichen/Unicodes
+    byte kleineZahl=100;    //Der Wertebereich sind ganze Zahlen von -128 bis 127
+    short Zahl=20000;   //Der Wertebereich sind ganze Zahlen von -32768 bis 32767
+    float einfacheGenauigkeit=2;    //Der Wertebereich sind Fließkommezahlen mit einfacher Genauigkeit
+
+    int Konstante=218; // Eine Variable ist eine Deklaration eines Typen ohne exate Wertzuweisung,
+        // d.h. einer Variablen können mehrere Werte zugewiesen werden ,
+        // während einer Konstante genaue Werte zugewiesen sind.
+    }
+}
