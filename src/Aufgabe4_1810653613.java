2c24634b5900d9677d9a3f4708f13576a7629dda
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-11-19 18:45:21 +0100

    Übung 4 Operatoren2

---
+++ src/Aufgabe4_1810653613.java
@@ -0,0 +1,19 @@
+public class Aufgabe4_1810653613
+{
+    public static void main(String[]args)
+    {
+       int zahl1=10;
+       int zahl2=200;
+        System.out.println(zahl1 < zahl2);
+        System.out.println(zahl2 < zahl1);
+        System.out.println(zahl1 > zahl2);
+        System.out.println(zahl2 > zahl1);
+        System.out.println(zahl1 <= zahl2 );
+        System.out.println(zahl2 <= zahl1);
+        System.out.println(zahl1 >= zahl2);
+        System.out.println(zahl2 >= zahl1);
+        System.out.println(zahl1 == zahl2);
+        System.out.println(zahl1 != zahl2);
+    }
+
+}
