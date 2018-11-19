e9fe20120ddaf98fa43786757e00024a3bac9571
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-11-19 18:25:41 +0100

    Übung 3 Operatoren 1

---
+++ src/Aufgabe3_1810653613.java
@@ -0,0 +1,20 @@
+public class Aufgabe3_1810653613 {
+
+        public static void main(String[]args)
+        {
+            int zahl1=20;
+            int zahl2=10;
+            int zahl3=5;
+            double zahl4=0.567;
+            double zahl5=0.989;
+            double zahl6=0.777;
+            System.out.println(zahl1 + zahl2);
+            System.out.println(zahl1 / zahl3);
+            System.out.println(zahl4 * zahl2);
+            System.out.println(zahl1 % zahl2);
+            System.out.println(++zahl3);
+            System.out.println(--zahl2);
+        }
+
+
+}
