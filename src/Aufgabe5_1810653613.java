6a703f58caea74e48448aa530f84cb98b87c276f
1810653613 <1810653613@stud.fh-kufstein.ac.at>
2018-11-20 18:39:21 +0100

    Übung 5 Inkrement und Dekrement von Variablen

---
+++ src/Aufgabe5_1810653613.java
@@ -0,0 +1,18 @@
+public class Aufgabe5_1810653613
+{
+    public static void main(String[]args)
+    {
+     int zahl1=180;
+     int zahl2=90;
+        System.out.println(zahl1++);
+        System.out.println(zahl2++);
+        System.out.println(zahl1);
+        System.out.println(zahl2);
+        System.out.println(++zahl1);
+        System.out.println(++zahl2);
+        System.out.println(zahl1);
+        System.out.println(zahl2);
+    }
+//gibt man in der Konsole zahl1++ aus wird zuerst zahl1 ausgegeben ohne veränderung und dann mit ++ 1 addiert, was die ausgabe der Variablen nach der Rechenaktion belegt.
+    // gibt man jedoch in der Konsole ++zahl1 ein so wird zuerst 1+ die Variable gerechnet und danach diese ausgegeben.
+}
